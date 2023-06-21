9189f0ddaac1a52353947852459688d65b47db1d
Angel Acuna <AngelAcuna2000@gmail.com>
2023-06-21 15:59:44 -0400

    Practice Complete

---
+++ DapperPractice/DapperCategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperPractice
+{
+    public class DapperCategoryRepository : ICategoryRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperCategoryRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories");
+        }
+
+        public void InsertCategories(string name)
+        {
+            _conn.Execute("INSERT INTO categories (Name) VALUES (@name)", 
+                new {name = name });
+        }
+    }
+}
--- DapperPractice/Program.cs
+++ DapperPractice/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using DapperPractice;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var categoryRepo = new DapperCategoryRepository(conn);
+
+var categories = categoryRepo.GetAllCategories();
+
+foreach (var category in categories)
+{
+    Console.WriteLine(category.CategoryID);
+    Console.WriteLine(category.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
